b94d6b6aad7a9dbb52e3b466e9767684ed1fa7db
kou <grimpoteuthistako@gmail.com>
2023-11-04 10:16:14 +0900

    やさしいKotlin入門37章37.2）オブジェクトとしての関数（無名関数の使用）

--- app/src/main/java/com/example/experiments/LearningRamda.kt
+++ app/src/main/java/com/example/experiments/LearningRamda.kt
@@ -23,6 +23,34 @@ class LearningRamda {
             val result = calculator(x, y)
             println("計算結果は${result}です")
         }
+
+        fun showCalculateResult() {
+            //関数を戻り値として受け取り、変数に格納する
+            val calculator1 = callCalculatorFunction("+")
+            val calculator2 = callCalculatorFunction("-")
+
+            val result1 = calculator1(10.0,20.0)
+            val result2 = calculator2(100.0,300.0)
+
+            println("計算結果は$result1")
+            println( "計算結果は$result2")
+        }
+
+        /**
+         * @param formulaType 計算式のタイプ
+         * @return  (x + y) または (x - y)　の計算をする関数を返す
+         */
+        private fun callCalculatorFunction(formulaType: String): (Double, Double) -> Double {
+            val plusCalculator = fun(x: Double, y: Double) = x + y
+            val minusCalculator = fun(x: Double, y: Double) = x - y
+
+            val calculator = when (formulaType) {
+                "+" -> plusCalculator
+                else -> minusCalculator
+            }
+            return calculator
+        }
+
     }
 
     //様々な無名関数の書き方
