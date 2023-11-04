7dc5149436e4b21b0f88a00a4176510d6f8bc19a
kou <grimpoteuthistako@gmail.com>
2023-11-04 10:14:52 +0900

    やさしいKotlin入門37章37.2）オブジェクトとしての関数の挙動確認(いろんな無名関数）

--- app/src/main/java/com/example/experiments/LearningRamda.kt
+++ app/src/main/java/com/example/experiments/LearningRamda.kt
@@ -24,4 +24,18 @@ class LearningRamda {
             println("計算結果は${result}です")
         }
     }
+
+    //様々な無名関数の書き方
+    val p1 = fun(x: Double, y: Double) = x + y
+    val p2 = fun(x: Double, y: Double): Double = x + y
+    val p3 = fun(x: Double, y: Double): Double {
+        return x + y
+    }
+
+    //これはかなり冗長な書き方な気がする。。
+    val p4: (Double, Double) -> Double = fun(x: Double, y: Double): Double {
+        return x + y
+    }
+
+    val p5: (Double, Double) -> Double = fun(x, y) = x + y
 }
